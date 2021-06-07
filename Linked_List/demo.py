ec255db9ca852d3b8dec22b827e2dbedf685cdff
Jinho Lee <jl5027@columbia.edu>
2021-06-07 23:10:34 +0900

    Create demo.py

---
+++ Linked_List/demo.py
@@ -0,0 +1,14 @@
+class Node:
+  def __init__(self, val=None):
+    self.val = val
+    self.next = None
+
+class LinkedList:
+  def __init__(self):
+    self.head = None
+  
+  def printList(self):
+    
+if __name__ == "__main__":
+  linked_list = LinkedList()
+  
