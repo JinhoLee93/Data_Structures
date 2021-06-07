8a4c44bd357b26dcd702a6db6e1b2962ef0f818c
Jinho Lee <jl5027@columbia.edu>
2021-06-07 23:14:55 +0900

    Update demo.py

--- Linked_List/demo.py
+++ Linked_List/demo.py
@@ -1,13 +1,17 @@
 class Node:
   def __init__(self, val=None):
     self.val = val
-    self.next = None
+    self.next_node = None
 
 class LinkedList:
   def __init__(self):
     self.head = None
   
-  def printList(self):
+  def print_list(self):
+    current_val = self.head
+    while current_val != None:
+      print(current_val)
+      current_val = current_val.next_node
     
 if __name__ == "__main__":
   linked_list = LinkedList()
