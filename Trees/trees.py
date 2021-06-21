e62b85609d8cc2d01a30bf21e9923fe706059cff
Jinho Lee <jl5027@columbia.edu>
2021-06-21 23:53:17 +0900

    Create trees.py

---
+++ Trees/trees.py
@@ -0,0 +1,12 @@
+Class TreeNode:
+  def __init__(self, val=0, left=None, right=None):
+    self.val = val
+    self.left = None
+    self.right = None
+    
+
+Class Tree:
+  def __init__(self):
+    root = None
+    
+ 
